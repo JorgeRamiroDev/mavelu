9f26905882d14a661fd4ee7cae106f4e7051eea6
Jorge Ramiro <102836967+JorgeRamiroDev@users.noreply.github.com>
2023-10-31 14:48:32 -0300

    views e urls

--- apps/cliente/models.py
+++ apps/cliente/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 
 class Configuracao(models.Model):
-    slogan = models.CharField(max_length=50)
-    nome_empresa = models.CharField(max_length=20)
-    telefone = models.CharField(max_length=20)
-    email = models.EmailField()
+   
 
     def __str__(self):
         return self.nome_empresa
--- apps/produto/admin.py
+++ apps/produto/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from .models import ItemOrder
+from .models import ItemOrder,Categorie,Fornecedor
 # Register your models here.
 @admin.register(ItemOrder)
 class ItemOrderAdmin(admin.ModelAdmin):
     list_display = ['nome_produto','preco']
     list_display_links = ['nome_produto']
+class ItemOrderInline(admin.TabularInline):
+    model = ItemOrder
+    extra = 0
+
+@admin.register(Categorie)
+class CategorieAdmin(admin.ModelAdmin):
+    list_display = ['nome_categorie',]
+    inlines = [ItemOrderInline]
+
+@admin.register(Fornecedor)
+class FornecedorAdmin(admin.ModelAdmin):
+    list_display = ['nome_fornecedor',]
